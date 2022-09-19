3d8702b57ff210a8abcafeae0f318c128b8606a8
Matheus Alberto <matheus.a.v.b@gmail.com>
2022-09-19 18:12:59 -0300

    first commit

---
+++ microservices/calculation/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`App running on http://localhost:${port}`);
+});
+
+app.use((req, res) => {
+	res.status(404).send('Not Found!');
+});
---
+++ microservices/store/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./src/routes/index');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+	console.log(`App running on http://localhost:${port}`);
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use((req, res) => {
+	res.status(404).send('Not Found!');
+});
+
+module.exports = app;
